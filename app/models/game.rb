cda916b340500ca7f2e89fbd1c789bd06e17527f
Chinar Regundwar <chinarregundwar@gmail.com>
2017-08-23 10:32:17 +0000

    relation changes for teams

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,11 @@
 class Game < ApplicationRecord
   belongs_to :match
-  belongs_to :team
+  belongs_to :team_a, :class_name => "Team", :foreign_key => "team_a_id"
+  belongs_to :team_b, :class_name => "Team", :foreign_key => "team_b_id"
   validates :score, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
+  after_save :set_match_details
+  
+  def set_match_details
+    
+  end
 end
